ce9ac0daffa33619699c8b00cdd8b136c8f9700b
Killyan Povoa <kpovoa@numericable.fr>
2023-01-11 15:27:11 +0100

    ajout de bonus dans le wrapper

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -2,10 +2,21 @@
 #include <string.h>
 
 int main(int argc, char **argv)
-{   
-    int (*funct)();
-    funct = (int (*)()) argv[1];
-    (int)(*funct)();
+{
+    if (argc < 2) {
+        printf("Veuillez indiquer un shellcode.");
+        return 1;
+    }
+    else {
+        printf("Taille du shellcode : %zu",  strlen(argv[1]));
+        printf("Shellcode : %s", argv[1]);
+
+        int (*funct)();
+        funct = (int (*)()) argv[1];
+        (int)(*funct)();
+
+        return 0;
+    }
     
-    return 0;
+    
 }
