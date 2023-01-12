7f7818b4f191a0022249e3865eefa3ebaf4eb9fc
Killyan Povoa <kpovoa@numericable.fr>
2023-01-12 14:41:18 +0100

    asm02

---
+++ asm32/asm02.s
@@ -0,0 +1,17 @@
+section .data
+    message db "1337"
+    len equ $-message
+
+section .text
+    global _start
+
+_start:
+    mov edx, len
+    mov ecx, message
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov eax, 1
+    mov ebx, 0
+    int 80h
